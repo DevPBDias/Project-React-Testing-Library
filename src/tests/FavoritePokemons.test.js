18562e4dd2b2e041d486b90e753b70f8002b6d57
devpbdias <devpbdias@gmail.com>
2022-05-09 19:43:04 -0300

    3rd req pronto

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Testando o componente "FavoritePokemons"', () => {
   it('Teste se é exibida na tela a mensagem "No favorite pokemon found"', () => {
     const { history } = renderWithRouter(<App />);
+
     history.push('/favorites');
     const msgNoFavorite = 'No favorite pokemon found';
     const msgEl = screen
@@ -15,11 +17,26 @@ describe('Testando o componente "FavoritePokemons"', () => {
   // para favoritar , tem q clicar more details e estar no home
   // na url do pokemon(pokemons/id) , tem q checkar na msg pokemon favorito
   it('Teste se são exibidos todos os cards de pokémons favoritados', () => {
-    const { history } = renderWithRouter(<App />);
+    const { history, debug } = renderWithRouter(<App />);
+
+    history.push('/');
+    const linkDetails = screen
+      .getByRole('link', { name: /More details/i });
+    expect(linkDetails).toBeInTheDocument();
+    userEvent.click(linkDetails);
+
+    const checkFavorite = screen.getByRole('checkbox', { checked: false });
+    expect(checkFavorite).toBeInTheDocument();
+    userEvent.click(checkFavorite);
+    debug();
+    console.log(checkFavorite);
+    // expect(checkFavorite).toHaveAttribute('checked', true);
+
+    // resolve o 7, raciocinio
     history.push('/favorites');
     const msgNoFavorite = 'No favorite pokemon found';
     const msgEl = screen
-      .getByText(msgNoFavorite, { selector: 'p' });
-    expect(msgEl).toBeInTheDocument();
+      .queryByText(msgNoFavorite, { selector: 'p' });
+    expect(msgEl).not.toBeInTheDocument();
   });
 });
